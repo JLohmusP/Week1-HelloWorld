430df18c16c2b4bfbf660f7535ad7e03e742bc46
Jaak Valter Lõhmuspuu <jlohmusp@tktk.ee>
2021-09-25 20:02:10 +0300

    Loo Tere Maailm

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tervist!");
+            Console.WriteLine("Mis on sinu nimi?");
+
+            string UserName;
+            UserName = Console.ReadLine();
+
+            Console.WriteLine("Tere, " + UserName);
+
+            Console.Read();
+        }
+    }
+}
